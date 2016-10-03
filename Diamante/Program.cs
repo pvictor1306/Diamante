f704071429cdbf172f6532250823e674abaa3e4b
Paulo Victor <02195609605@est.udicentro.local>
2016-10-03 11:20:20 -0300

    Fazendo os primeiros testes de movimentacao

--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -8,14 +8,21 @@ namespace Diamante
         static void Main(string[] args)
         {
             try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                while (!partida.terminada) {
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
             }
             catch(TabuleiroException e) {
                 Console.WriteLine(e.Message);
--- Diamante/Tabuleiro/Peca.cs
+++ Diamante/Tabuleiro/Peca.cs
@@ -16,5 +16,9 @@ namespace tabuleiro
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos() {
+            qteMovimentos++;
+        }
     }
 }
