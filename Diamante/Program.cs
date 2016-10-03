3e8fce2afce8e4ebf4185be072c3a18ab0b4f689
Paulo Victor <02195609605@est.udicentro.local>
2016-10-03 08:55:55 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -7,6 +7,9 @@ namespace Diamante
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
