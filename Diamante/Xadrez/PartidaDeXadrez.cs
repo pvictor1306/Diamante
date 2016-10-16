baff449f0a7240fb9e31bdcbbd673ff1aa31f3f3
pvictor1306 <paulovictor.joao@gmail.com>
2016-10-16 18:26:58 -0200

    Correção - Renomeando o método para movimentoPossivel

--- Diamante/Xadrez/PartidaDeXadrez.cs
+++ Diamante/Xadrez/PartidaDeXadrez.cs
@@ -59,9 +59,12 @@ namespace Xadrez {
                 xeque = false;
             }
 
+            if (testeXequemate(adversaria(jogadorAtual))) {
+                terminada = true;
+            }
+            else {
             turno++;
             mudaJogador();
-
         }
 
         public void validarPosicaoDeOrigem(Posicao pos) {
@@ -77,7 +80,7 @@ namespace Xadrez {
         }
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
-            if (!tab.peca(origem).podeMoverPara(destino)) {
+            if (!tab.peca(origem).movimentoPossivel(destino)) {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
         }
@@ -143,6 +146,30 @@ namespace Xadrez {
                 }
             }
             return false;
+        }
+
+        public bool testeXequemate(Cor cor) {
+            if (!estaEmXeque(cor)) {
+                return false;
+            }
+            foreach(Peca x in pecasEmJogo(cor)) {
+                bool[,] mat = x.movimentosPossiveis();
+                for(int i = 0; i < tab.linhas; i++) {
+                    for(int j = 0; j < tab.colunas; j++) {
+                        if(mat[i, j]) {
+                            Posicao origem = x.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         public void colocarNovaPeca(char coluna, int linha, Peca peca) {
