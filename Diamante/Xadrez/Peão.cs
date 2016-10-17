1c13d8eea931c4d16eb19dd72f39920d7dbacdcd
pvictor1306 <paulovictor.joao@gmail.com>
2016-10-17 09:51:18 -0200

    Jogada especial roque pequeno e roque grande

--- Diamante/Xadrez/Peão.cs
+++ Diamante/Xadrez/Peão.cs
@@ -10,7 +10,7 @@ namespace Xadrez {
 
         private bool existeInimigo(Posicao pos) {
             Peca p = tab.peca(pos);
-            return p == null && p.cor != cor;
+            return p != null && p.cor != cor;
         }
         private bool livre(Posicao pos) {
             return tab.peca(pos) == null;
