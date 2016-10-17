ad1de11db3e318d6f4227afc4c9b8b15c7fd2d27
pvictor1306 <paulovictor.joao@gmail.com>
2016-10-17 11:12:18 -0200

    Jogada especial promoção

--- Diamante/Xadrez/PartidaDeXadrez.cs
+++ Diamante/Xadrez/PartidaDeXadrez.cs
@@ -125,6 +125,19 @@ namespace Xadrez {
                 throw new TabuleiroException("VocÊ não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if(p is Peao) {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
